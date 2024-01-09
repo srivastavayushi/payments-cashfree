71f211c52b1dfe1bb9db40dd387a3990384b4ea0
Ayushi <ayushimanoj1201@gmail.com>
2024-01-09 19:57:59 +0530

    update font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        customFont: ['"Inter"', "sans-serif"],
+        customFont: ["Inter", "sans-serif"],
       },
     },
   },
